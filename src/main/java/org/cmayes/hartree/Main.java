e3ffa38e6f76e52acd279ba6c864ac5649a48a59
Chris Mayes <cmayes@cmayes.com>
2011-11-21 20:41:57 -0600

    Added main class and logback.
    NEW - task 29: Hartree can process a single Gaussian file through the
    CLI 
    http://localhost:8088/issues/show/29

---
+++ src/main/java/org/cmayes/hartree/Main.java
@@ -0,0 +1,107 @@
+package org.cmayes.hartree;
+
+import static org.kohsuke.args4j.ExampleMode.ALL;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This is the main entry point for running Hartree CLI functions.
+ * 
+ * @author cmayes
+ */
+public class Main {
+    /** Logger. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    /** Receives leftover command line parameters. */
+    @Argument
+    private List<String> arguments = new ArrayList<String>();
+    private File file;
+    private File directory;
+
+    /**
+     * Sets the file if it is readable.
+     * 
+     * @param theFile
+     *            The file to read.
+     * @throws IllegalArgumentException
+     *             If the file is not readable.
+     */
+    @Option(name = "-file", usage = "The file to process")
+    public void setFile(final File theFile) {
+        if (theFile.canRead()) {
+            this.file = theFile;
+        } else {
+            throw new IllegalArgumentException(String.format(
+                    "File %s is not readable", theFile.getAbsolutePath()));
+        }
+    }
+
+    /**
+     * Sets the directory if it is a readable directory.
+     * 
+     * @param dir
+     *            The base directory to start from.
+     */
+    @Option(name = "-directory", usage = "The base directory of the files to process")
+    public void setDirectory(final File dir) {
+        if (dir.canRead() && dir.isDirectory()) {
+            this.directory = dir;
+        } else {
+            throw new IllegalArgumentException(String.format(
+                    "Directory %s is not readable or is not a directory",
+                    dir.getAbsolutePath()));
+        }
+    }
+
+    /**
+     * @param args
+     *            The CLI arguments.
+     */
+    public static void main(final String[] args) {
+        try {
+            new Main().doMain(args);
+        } catch (final CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err
+                    .println("java org.cmayes.hartree.Main [options...] arguments...");
+            e.getParser().printUsage(System.err);
+            System.err.println();
+            System.err.println("  Example: java org.cmayes.hartree.Main "
+                    + e.getParser().printExample(ALL));
+            System.exit(1);
+        } catch (final Exception e) {
+            LOGGER.error("Top-level exception caught", e);
+            System.err.println(e.getMessage());
+            System.exit(2);
+        }
+    }
+
+    /**
+     * Performs argument processing and command dispatching.
+     * 
+     * @param args
+     *            The arguments to process.
+     * @throws CmdLineException
+     *             When there are problems processing the command line.
+     */
+    public void doMain(final String[] args) throws CmdLineException {
+        final CmdLineParser parser = new CmdLineParser(this);
+
+        parser.parseArgument(args);
+
+        // after parsing arguments, you should check
+        // if enough arguments are given.
+        // if (arguments.isEmpty()) {
+        // throw new CmdLineException(parser, "No argument is given");
+        // }
+    }
+}
